1ce96bc1e0e7bcd1f07c2ad14a315d7a4c4b1f8a
apaz-mar <aaron.paz.martinez@gmail.com>
2025-12-13 21:31:17 +0100

    Add files via upload

---
+++ cpp04/ex01/Animal.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apaz-mar <apaz-mar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/07 20:14:32 by apaz-mar          #+#    #+#             */
+/*   Updated: 2025/12/11 00:38:35 by apaz-mar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <string>
+#include <iostream>
+
+class Animal
+{
+    protected:
+        std::string type;
+
+    public:
+        Animal();
+        Animal(const Animal &other);
+        virtual ~Animal();
+
+        Animal &operator=(const Animal &other);
+
+        std::string const &getType() const;
+        virtual void makeSound() const = 0;         //= 0 makes it abstract, not possible to create an Animal object
+};
+
+#endif
---
+++ cpp04/ex01/Dog.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apaz-mar <apaz-mar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/07 21:34:29 by apaz-mar          #+#    #+#             */
+/*   Updated: 2025/12/13 21:28:16 by apaz-mar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Dog.hpp"
+
+// Dog allocates a Brain in constructor, deletes its Brain in destructor. Copy constructor allocates a new Brain, Assignment copies Brain contents
+Dog::Dog() : brain(new Brain())
+{
+    std::cout << "Dog default constructor called" << std::endl;
+    this->type = "Dog";
+}
+
+Dog::Dog(const Dog &other) : Animal(other)
+{
+    brain = new Brain(*other.brain);            //copy of the content for deep copy
+    std::cout << "Dog copy constructor called" << std::endl;
+    this->type = other.type;
+}
+
+Dog::~Dog()
+{
+    std::cout << "Dog destructor called" << std::endl;
+    delete brain;
+}
+
+Dog &Dog::operator=(const Dog &other)
+{
+    std::cout << "Dog copy assignment operator called" << std::endl;
+    if (this != &other)
+    {
+        Animal::operator=(other);
+        brain = new Brain(*other.brain);            //copy of the content for deep copy
+    }
+    return (*this);
+}
+
+void Dog::makeSound() const
+{
+    std::cout << "Woof! Woof!" << std::endl;
+}
+
+Brain* Dog::getBrain() const
+{
+    return (brain);
+}
